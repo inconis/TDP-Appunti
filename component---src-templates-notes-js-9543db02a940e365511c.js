502585b7ab069550aaa348fc3b181d3d05d8dc3e
Riccardo <riccardo.io2@gmail.com>
2020-04-27 15:35:16 +0200

    Updates

---
+++ component---src-templates-notes-js-9543db02a940e365511c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{Tir5:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return o}));var r=n("q1tI"),a=n.n(r),l=n("Bl7J");t.default=function(e){var t=e.data.markdownRemark;console.log(t);var n=t.frontmatter.title;return a.a.createElement(l.a,null,a.a.createElement("h2",{style:{marginTop:"1.45rem"}},n),a.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))};var o="2663108596"}}]);
+//# sourceMappingURL=component---src-templates-notes-js-9543db02a940e365511c.js.map
